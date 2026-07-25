3ab28afdd58c65f42f05405a38971837fc3c90e7
satyamgupta12 <72330494+satyamgupta12@users.noreply.github.com>
2026-07-25 17:42:54 +0530

    Time: 0 ms (100%), Space: 9.8 MB (26.15%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int n=s.length();
+        int m=t.length();
+        if(n!=m) return false;
+        vector<int>alpha(26,0);
+        for(int i=0;i<n;i++){
+            alpha[s[i] - 'a']++;
+            alpha[t[i] - 'a']--;
+        }
+        
+        for(int i=0;i<26;i++){
+            if(alpha[i]!=0) return false;
+        }
+        return true;
+    }
+};
