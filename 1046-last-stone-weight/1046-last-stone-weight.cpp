9338eac0d962938020a9a273cfcdded5ebf66dac
satyamgupta12 <72330494+satyamgupta12@users.noreply.github.com>
2026-07-30 12:22:45 +0530

    Time: 0 ms (100%), Space: 9.8 MB (98.46%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.cpp
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     int lastStoneWeight(vector<int>& stones) {
-        priority_queue<int> pq;
-        for(auto it:stones){
-            pq.push(it);
-        }
+        //priority_queue<int> pq; tales o(nlogn ) time 
+        priority_queue<int> pq(stones.begin(), stones.end());  //takes o(n) time 
+    
         while(pq.size()>=2){
             int first=pq.top();
             pq.pop();
