6bd6120a6f3171ea9115717aabc3159cd9b309ff
satyamgupta12 <72330494+satyamgupta12@users.noreply.github.com>
2026-08-13 10:41:27 +0530

    Time: 23 ms (52.08%), Space: 70.5 MB (48.75%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -4,21 +4,23 @@ public:
         int n=bloomDay.size(),low=*min_element(bloomDay.begin(),bloomDay.end());
         int high = *max_element(bloomDay.begin(),bloomDay.end());
         int days=-1;
+        if(1ll* m * k > n)
+            return -1;
         while(low<=high){
-            int cnt=0,bou=0;
+            int conse=0,bou=0;
             int mid=low+(high-low)/2;
             for(int i=0;i<n;i++){
                 if(bloomDay[i]<=mid){
-                    cnt++;
+                    conse++;
 
                 }
                 else{
-                    bou=bou+(cnt/k);
-                    cnt=0;
+                    bou=bou+(conse/k);
+                    conse=0;
                 }
                 
             }
-            bou=bou+(cnt/k);
+            bou=bou+(conse/k);
             if(bou<m){
                 low=mid+1;
             }
